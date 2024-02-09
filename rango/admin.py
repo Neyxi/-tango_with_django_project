0d3a9ab76fee9fcf1b50e0da53160c2f5f1631f8
Boyang An <2504413a@student.gla.ac.uk>
2024-02-09 01:22:54 +0000

    first commit

--- rango/admin.py
+++ rango/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from rango.models import Category, Page
-from newDjangoApp.models import MyNewModel
+from rango.models import MyNewModel
+
+class PageAdmin(admin.ModelAdmin):
+    list_display = ('title', 'category', 'url')
 
 admin.site.register(Category)
-admin.site.register(Page)
+admin.site.register(Page, PageAdmin)
 admin.site.register(MyNewModel)
--- rango/models.py
+++ rango/models.py
@@ -10,6 +10,11 @@ class MyNewModel(models.Model):
 
 class Category(models.Model):
     name = models.CharField(max_length=128, unique=True)
+    views = models.IntegerField(default=0)
+    likes = models.IntegerField(default=0)
+    
+    def __str__(self):
+        return self.name
     
     class Meta:
         verbose_name_plural = "Categories"
