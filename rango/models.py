f23a07883042cf47747a8299f5394266d572c05c
Boyang An <2504413a@student.gla.ac.uk>
2024-02-09 02:25:23 +0000

    first commit

--- rango/models.py
+++ rango/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.utils.text import slugify
+
 
 class MyNewModel(models.Model):
     # Add a new field to the model
@@ -12,12 +14,14 @@ class Category(models.Model):
     name = models.CharField(max_length=128, unique=True)
     views = models.IntegerField(default=0)
     likes = models.IntegerField(default=0)
+    slug = models.SlugField()
     
-    def __str__(self):
-        return self.name
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super(Category, self).save(*args, **kwargs)
     
     class Meta:
-        verbose_name_plural = "Categories"
+        verbose_name_plural = "categories"
 
     def __str__(self):
         return self.name
