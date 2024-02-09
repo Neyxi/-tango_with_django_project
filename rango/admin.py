272419f0433eee0d8670cb6f01e5473798e72c85
Boyang An <2504413a@student.gla.ac.uk>
2024-02-09 00:49:09 +0000

    first commit

--- rango/admin.py
+++ rango/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from rango.models import Category, Page
+from .models import MyNewModel
 
 admin.site.register(Category)
 admin.site.register(Page)
+admin.site.register(MyNewModel)
--- rango/models.py
+++ rango/models.py
@@ -1,5 +1,13 @@
 from django.db import models
 
+class MyNewModel(models.Model):
+    # Add a new field to the model
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    
+    def __str__(self):
+        return self.name
+
 class Category(models.Model):
     name = models.CharField(max_length=128, unique=True)
     
